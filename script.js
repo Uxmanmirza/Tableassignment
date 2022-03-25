321f75b10f2765b57594e14a328862ed600f378c
Usman Ali <uxmanmirza1050@gmail.com>
2022-03-26 03:39:25 +0500

    all done

--- script.js
+++ script.js
@@ -22,41 +22,37 @@ document.getElementById('body').appendChild(table);
 
 let row_1 = document.createElement('tr');
 let heading_1 = document.createElement('th');
-heading_1.innerHTML = "Sr. No.";
+heading_1.innerHTML = "ID";
 let heading_2 = document.createElement('th');
 heading_2.innerHTML = "Name";
 let heading_3 = document.createElement('th');
 heading_3.innerHTML = "FatherName";
 let heading_4 = document.createElement('th');
-heading_4.innerHTML = "ID";
-let heading_5 = document.createElement('th');
-heading_5.innerHTML = "CNIC";
+heading_4.innerHTML = "CNIC";
+ 
 
 row_1.appendChild(heading_1);
 row_1.appendChild(heading_2);
 row_1.appendChild(heading_3);
 row_1.appendChild(heading_4);
-row_1.appendChild(heading_5);
 thead.appendChild(row_1);
 
 
 let row_2 = document.createElement('tr');
 let row_2_data_1 = document.createElement('td');
-row_2_data_1.innerHTML =  Math.floor(Math.random() * 11);
+row_2_data_1.innerHTML =  ID.value;
 let row_2_data_2 = document.createElement('td');
 row_2_data_2.innerText = Name.value;
 let row_2_data_3 = document.createElement('td');
 row_2_data_3.innerText = FName.value;
 let row_2_data_4 = document.createElement('td');
-row_2_data_4.innerText = ID.value;
-let row_2_data_5 = document.createElement('td');
-row_2_data_5.innerText = CNIC.value;
+row_2_data_4.innerText = CNIC.value;
+ 
 
 row_2.appendChild(row_2_data_1);
 row_2.appendChild(row_2_data_2);
 row_2.appendChild(row_2_data_3);
 row_2.appendChild(row_2_data_4);
-row_2.appendChild(row_2_data_5);
 tbody.appendChild(row_2);
 
 
